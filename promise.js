4718d27d9d2c9a90d4fd4192cb73acbe11f31c51
tpwiedy <wiedy.pratama@gmail.com>
2022-06-12 23:21:35 +0700

    feat: Challenge 7 Done

--- promise.js
+++ promise.js
@@ -1,7 +1,25 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (kondisi) => {
+  let data = [];
+  try {
+    let data1 = await promiseTheaterIXX();
+    let data2 = await promiseTheaterVGC();
+    data1.forEach((element) => {
+      data.push(element);
+    });
+    data2.forEach((element) => {
+      data.push(element);
+    });
+    let redu = data
+      .filter((d) => d.hasil === kondisi)
+      .reduce((p, c, i, a) => i + 1);
+    return redu;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
